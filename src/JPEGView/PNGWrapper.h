aad90420fa006957f78d99b74aba69a155bc3ad5
qbnu <93988953+qbnu@users.noreply.github.com>
2022-12-19 15:13:48 -0500

    PNGWrapper.h

--- src/JPEGView/PNGWrapper.h
+++ src/JPEGView/PNGWrapper.h
@@ -12,8 +12,8 @@ public:
 		int& frame_count, // number of frames
 		int& frame_time, // frame duration in milliseconds
 		bool& outOfMemory, // set to true when no memory to read image
-		const void* buffer, // memory address containing jpeg compressed data.
-		int sizebytes); // size of jpeg compressed data
+		FILE* file // memory address containing jpeg compressed data.
+		); // size of jpeg compressed data
 
 	static void DeleteCache();
 };
